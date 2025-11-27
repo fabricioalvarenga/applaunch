8736ce11755a12594fbbce57d67aa25f542f16f2
Fabricio Alvarenga <fab.oliveira.alvarenga@gmail.com>
2025-11-26 22:38:07 -0300

    Create app info structure

--- AppLaunch/Preview Content/Models/AppInfo.swift
+++ AppLaunch/Preview Content/Models/AppInfo.swift
@@ -13,4 +13,12 @@ struct AppInfo: Identifiable {
     let icon: NSImage
     let bundleURL: URL
     let bundleIdentifier: String?
+    
+    init(id: UUID = UUID(), name: String, icon: NSImage, bundleURL: URL, bundleIdentifier: String? = nil) {
+        self.id = id
+        self.name = name
+        self.icon = icon
+        self.bundleURL = bundleURL
+        self.bundleIdentifier = bundleIdentifier
+    }
 }
